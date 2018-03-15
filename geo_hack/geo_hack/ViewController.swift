99ae0f163cbf1f60e343fbe66b7bb0c6d7771be4
Johnnie Tran <johnnie.tran88@gmail.com>
2018-03-15 16:33:36 -0700

    enabled location services

--- geo_hack/geo_hack/ViewController.swift
+++ geo_hack/geo_hack/ViewController.swift
@@ -7,14 +7,62 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate{
+    
+    let locationManager = CLLocationManager()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
+
+        func enableBasicLocationServices() {
+            locationManager.delegate = self
+            
+            switch CLLocationManager.authorizationStatus() {
+            case .notDetermined:
+                // Request when-in-use authorization initially
+                locationManager.requestWhenInUseAuthorization()
+                print("request")
+                break
+                
+            case .restricted, .denied:
+                // Disable location features
+//                disableMyLocationBasedFeatures()
+                break
+                
+            case .authorizedWhenInUse, .authorizedAlways:
+                print("authorized")
+                // Enable location features
+//                enableMyWhenInUseFeatures()
+                break
+            }
+        }
+        enableBasicLocationServices()
+        
+        func checkForLocationServices() {
+            if CLLocationManager.locationServicesEnabled() {
+                print("Location services enable")
+            } else {
+                print("Location services disabled")
+            }
+        }
+        
+        checkForLocationServices()
+        locationManager.startUpdatingLocation()
+        
+    }
+        // Do any additional setup after loading the view, typically from a nib.
+    
+    @IBAction func getLocation(_ sender: UIButton) {
+        print(locationManager.location)
+
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print(locations[locations.count-1])
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
